f76aa92d6cf1690fb357604560069924efe3d6d2
windluffy <windnarsas@gmail.com>
2015-12-31 02:44:07 +0700

    FlatLab Template

---
+++ source/src/main/java/vn/aliviet/order/rest/UserResource.java
@@ -0,0 +1,7 @@
+package vn.aliviet.order.rest;
+
+/**
+ * Created by windluffy on 29/12/2015.
+ */
+public class UserResource {
+}
---
+++ source/src/main/java/vn/aliviet/order/service/UserService.java
@@ -0,0 +1,7 @@
+package vn.aliviet.order.service;
+
+/**
+ * Created by windluffy on 29/12/2015.
+ */
+public class UserService {
+}
