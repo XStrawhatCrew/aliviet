81370a3b940f8ee55056061764204054b89b3b93
windluffy <windnarsas@gmail.com>
2016-02-23 17:39:39 +0700

    load data from crawler + html + css

--- source/src/main/webapp/js/controllers/OrderController.js
+++ source/src/main/webapp/js/controllers/OrderController.js
@@ -8,13 +8,6 @@ app.controller('OrderController', ['$scope', '$rootScope', 'ProductCrawlerServic
     $scope.inputLink = "";
 
 
-
-
-
-
-
-
-
     $scope.makeANewProductObj = function () {
         var obj = new Object();
         obj.linkSource = "";
@@ -30,17 +23,17 @@ app.controller('OrderController', ['$scope', '$rootScope', 'ProductCrawlerServic
         return obj;
     };
 
-    $scope.createOrder = function (isvalid,order) {
-        if(!isvalid) return;
+    $scope.createOrder = function (isvalid, order) {
+        if (!isvalid) return;
         console.log(order);
     };
 
     $scope.createOrders = function (orders) {
-        if($scope.orderForm.$valid){
+        if ($scope.orderForm.$valid) {
             console.log(orders);
             $rootScope.orders = orders;
 
-        }else{
+        } else {
             toastr.error("Vui lòng điền đầy đủ những đơn còn trống !");
         }
 
@@ -64,9 +57,44 @@ app.controller('OrderController', ['$scope', '$rootScope', 'ProductCrawlerServic
                 obj.featureImage = result.feature_image;
                 obj.productName = result.product_name;
                 obj.shopName = result.shop_name;
+                obj.colors = result.colors;
+
+                if($scope.orders.length == 0) {
+                    $scope.orders.push([]);
+                    $scope.orders[0].push(obj);
+                } else if ($scope.orders.length == 1 && $scope.orders[0].length == 0) {
+                    $scope.orders[0].push(obj);
+                } else {
+                    var indexOrder = $scope.getIndexOrderSameShop(obj.shopName);
+                    console.log("indexOrder:" + indexOrder);
+                    if (indexOrder == -1) {
+                        var newOrder = [];
+                        newOrder.push(obj);
+                        $scope.orders.push(newOrder);
+                    } else {
+                        $scope.orders[indexOrder].push(obj);
+                    }
+                }
+                console.log($scope.orders);
+
             }
         );
 
+    };
+
+    $scope.isShowColorSelection = function(colors) {
+        return colors.type == 'text';
+    };
+
+    $scope.getIndexOrderSameShop = function (shopName) {
+        for (var i = 0; i < $scope.orders.length; i++) {
+            for (var j = 0; j < $scope.orders[i].length; j++) {
+                if (shopName == $scope.orders[i][j].shopName) {
+                    return i;
+                }
+            }
+        }
+        return -1;
     };
 
     $scope.removeProduct = function (order, indexProduct, indexOrder) {
@@ -87,10 +115,12 @@ app.controller('OrderController', ['$scope', '$rootScope', 'ProductCrawlerServic
         $scope.orders.push($scope.makeANewOrderObj());
     };
 
+    $scope.getShopName = function(order) {
+        return order[0].shopName;
+    };
 
     $scope.init = function () {
-        var newOrder = $scope.makeANewOrderObj();
-        $scope.orders.push(newOrder);
+
     };
 
     $scope.init();
